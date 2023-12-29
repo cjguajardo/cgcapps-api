22b222524e0a3935a9a6d34a4e5882ff8f1dd275
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2023-12-29 15:59:28 -0300

    Update database schema and add origin retrieval functionality

--- src/database/migrations.sql
+++ src/database/migrations.sql
@@ -1,14 +1,26 @@
-DROP TABLE IF NOT EXISTS `geo_ip`;
+DROP TABLE IF NOT EXISTS "geo_ip";
 
-CREATE TABLE `geo_ip` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ip` varchar(15) unsigned NOT NULL,
-  `continent` varchar(64) NOT NULL,
-  `country` varchar(64) NOT NULL,
-  `region` varchar(128) NOT NULL,
-  `city` varchar(128) NOT NULL,
-  `latitude` float NOT NULL,
-  `longitude` float NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE "geo_ip" (
+  "id" int(10) unsigned NOT NULL AUTO_INCREMENT,
+  "ip" varchar(15) unsigned NOT NULL,
+  "continent" varchar(64) NOT NULL,
+  "country" varchar(64) NOT NULL,
+  "region" varchar(128) NOT NULL,
+  "city" varchar(128) NOT NULL,
+  "latitude" float NOT NULL,
+  "longitude" float NOT NULL,
+  "created_at" timestamp NOKT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ("id"),
+)
+
+
+drop table if exists "allowed_origins";
+
+create table
+  "allowed_origins" (
+    "id" bigint primary key generated always as identity,
+    "origin" text not null,
+    "enabled" boolean not null default true,
+    "endpoint" text not null,
+    "created_at" timestamp with time zone not null default current_timestamp
+  );
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -7,33 +7,57 @@ export const supabase = createClient(
   process.env.SUPABASE_ANON_KEY ?? ''
 )
 
-export const insertGeoData = async ( data: GeoDataType ): Promise<void> => {
-  const result = await supabase.from( 'geo_ip' ).insert( [data] )
-  console.log( { result } )
-  if ( result.error ) {
-    console.error( { error: result.error } )
+export const insertGeoData = async (data: GeoDataType): Promise<void> => {
+  const result = await supabase.from('geo_ip').insert([data])
+  console.log({ result })
+  if (result.error) {
+    console.error({ error: result.error })
   }
 }
 
 export const getLastGeoData = async (): Promise<GeoDataResponse | null> => {
   const { data, error } = await supabase
-    .from( 'geo_ip' )
-    .select( '*' )
-    .order( 'created_at', { ascending: false } )
-    .limit( 1 )
+    .from('geo_ip')
+    .select('*')
+    .order('created_at', { ascending: false })
+    .limit(1)
 
-  if ( error ) {
-    console.error( { error } )
-    return null;
+  if (error) {
+    console.error({ error })
+    return null
   }
 
-  if ( !data ) {
+  if (!data) {
     return null
   }
 
   const adapter = new GeoDataAdapter()
-  const geoData = adapter.extractDataForResponse( data[0] )
-  console.log( { data: data[0] } );
+  const geoData = adapter.extractDataForResponse(data[0])
+  console.log({ data: data[0] })
 
   return geoData
+}
+
+export const getOrigins = async (endpoint?: string | null): Promise<string[]> => {
+  let endpoints = '("/geo","/mail")'
+  if (endpoint) {
+    endpoints = `("${endpoint}")`
+  }
+
+  const { data, error } = await supabase.from('allowed_origins')
+    .select('*')
+    .filter('enabled', 'eq', true)
+    .filter('endpoint', 'in', endpoints)
+    .order('created_at', { ascending: false })
+
+  if (error) {
+    console.error({ error })
+    return []
+  }
+
+  if (!data) {
+    return []
+  }
+
+  return Array.from(new Set(data.map(item => item.origin)).values())
 }
