69a53023c7b4d6c7c7749493652040621eb89a8a
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-01-06 00:46:57 -0300

    Update API headers configuration

--- next.config.js
+++ next.config.js
@@ -3,9 +3,23 @@ const nextConfig = {
   async headers () {
     return [
       {
-        source: '/api/:path*',
+        source: '/api/ip',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          // Allow for specific domains to have access or * for all
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
         ]
       }
     ]
