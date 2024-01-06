53a8912ed54244c9aa62ca4c16a7d5bf132b4b4d
Carlos Guajardo <cj.guajardo@cgcapps.cl>
2024-01-06 02:41:38 -0300

    Add next-cors middleware for CORS handling

--- next.config.js
+++ next.config.js
@@ -1,29 +1,13 @@
 /** @type {import('next').NextConfig} */
+const nextCors = require('nextjs-cors')
 const nextConfig = {
-  async headers () {
-    return [
-      {
-        source: '/api/ip',
-        headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ]
-      }
-    ]
-  }
+  middleware: [
+    nextCors({
+      methods: ['GET', 'POST'],
+      origin: ['https://cgcapps.cl'],
+      optionsSuccessStatus: 200
+    })
+  ]
 }
 
 module.exports = nextConfig
